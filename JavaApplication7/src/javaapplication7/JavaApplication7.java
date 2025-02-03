4bea6edb737cdc2ee23f2664d75e9bb8cb30353d
oyvaro <oyvaro@gmail.com>
2025-02-03 23:23:44 +0100

    fuck off world

--- JavaApplication7/src/javaapplication7/JavaApplication7.java
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -15,6 +15,8 @@ public class JavaApplication7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //fuck off buddy
     }
     
 }
